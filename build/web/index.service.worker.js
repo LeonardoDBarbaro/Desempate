e559eb027c2a7321bfbb7fe405f81b154e05eac7
LEO <89648679+LeonardoDBarbaro@users.noreply.github.com>
2024-07-19 06:30:21 -0300

    ds
    d

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1721381083|7733840154';
+const CACHE_VERSION = '1721381372|8022791727';
 /** @type {string} */
 const CACHE_PREFIX = 'Desempate-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
